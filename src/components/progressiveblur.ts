1084e44de19e7471a780baba1fec9ff5506f04b7
Oskar Sherrah <86351848+byOskar@users.noreply.github.com>
2024-08-23 21:58:39 +1000

    feat: parse prop value with px suffix (#1)

--- src/components/progressiveblur.ts
+++ src/components/progressiveblur.ts
@@ -5,13 +5,25 @@ export interface ProgressiveBlurOptions {
     borderRadius: number,
 }
 
+function parsePropValue(value: string | number | undefined): number {
+    if (typeof value === 'string') {
+        value = value.replace('px', '')
+        return parseInt(value);
+    } else if (typeof value === 'number') {
+        return value;
+    } else {
+        return 0;
+    }
+}
+
 export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
     name: 'ProgressiveBlur',
     props: ['blur', 'borderRadius'] as unknown as undefined,
     setup(props) {
         return () => {
-            props.blur = props.blur || 0;
-            props.borderRadius = props.borderRadius || 0;
+
+            const _blur = parsePropValue(props.blur);
+            const _borderRadius = parsePropValue(props.borderRadius);
 
             return h('div',
                 {
@@ -27,7 +39,7 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                         position: 'relative',
                         width: '100%',
                         height: '100%',
-                        borderRadius: props.borderRadius + 'px',
+                        borderRadius: _borderRadius + 'px',
                     },
                 },
                 [
@@ -38,11 +50,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 1,
-                                backdropFilter: 'blur(' + props.blur / (2**7) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**7) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**7) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**7) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 0) 37.5%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 0) 37.5%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -54,11 +66,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 2,
-                                backdropFilter: 'blur(' + props.blur / (2**6) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**6) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**6) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**6) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 0) 50%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 0) 50%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -70,11 +82,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 3,
-                                backdropFilter: 'blur(' + props.blur / (2**5) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**5) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**5) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**5) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 62.5%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 62.5%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -86,11 +98,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 4,
-                                backdropFilter: 'blur(' + props.blur / (2**4) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**4) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**4) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**4) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 0) 75%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 0) 75%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -102,11 +114,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 5,
-                                backdropFilter: 'blur(' + props.blur / (2**3) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**3) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**3) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**3) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 87.5%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 87.5%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -118,11 +130,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 6,
-                                backdropFilter: 'blur(' + props.blur / (2**2) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**2) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**2) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**2) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 0) 100%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 0) 100%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -134,11 +146,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 7,
-                                backdropFilter: 'blur(' + props.blur / (2**1) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**1) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**1) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**1) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 1) 100%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 1) 100%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
@@ -150,11 +162,11 @@ export const ProgressiveBlur = defineComponent<ProgressiveBlurOptions>({
                                 position: 'absolute',
                                 inset: 0,
                                 zIndex: 8,
-                                backdropFilter: 'blur(' + props.blur / (2**0) + 'px)',
-                                WebkitBackdropFilter: 'blur(' + props.blur / (2**0) + 'px)',
+                                backdropFilter: 'blur(' + _blur / (2**0) + 'px)',
+                                WebkitBackdropFilter: 'blur(' + _blur / (2**0) + 'px)',
                                 maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(0, 0, 0, 1) 100%)',
                                 WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(0, 0, 0, 1) 100%)',
-                                borderRadius: props.borderRadius + 'px',
+                                borderRadius: _borderRadius + 'px',
                                 pointerEvents: 'none',
                             },
                         },
